91654292de7a2878fc4a06fa881238256b312249
Shreeram Murali <shreeram.m@gmail.com>
2024-09-18 17:35:09 +0300

    dataset created

--- dataset_prep.py
+++ dataset_prep.py
@@ -12,9 +12,11 @@ def value_to_float(x):
     """
     if type(x) == float or type(x) == int:
         return x
-    if 'K' in x:
+    if type(x) == str and x.isnumeric():
+        return float(x)
+    if 'k' in x:
         if len(x) > 1:
-            return float(x.replace('K', '')) * 1000
+            return float(x.replace('k', '')) * 1000
         return 1000.0
     if 'M' in x:
         if len(x) > 1:
@@ -22,7 +24,8 @@ def value_to_float(x):
         return 1000000.0
     if 'B' in x:
         return float(x.replace('B', '')) * 1000000000
-    return 0.0
+    
+    return 0.
 
 def main():
     # Read the CSV files
@@ -31,7 +34,6 @@ def main():
     lex_df = pd.read_csv('dataset/lex.csv')
     pop_20_39_df = pd.read_csv('dataset/population_aged_20_39_years_both_sexes_percent.csv')
     gdp_df = pd.read_csv('dataset/gdp_pcap.csv')
-    hosts_df = pd.read_csv('dataset/olympic_hosts.csv')
 
     # Melt the dataframes to have year as a column
     pop_df = pd.melt(pop_df, id_vars=['country'], var_name='year', value_name='population')
@@ -53,14 +55,14 @@ def main():
         final_df = final_df.merge(df, left_on=['country_name', 'year'], right_on=['country', 'year'], how='left')
         final_df = final_df.drop('country', axis=1)  # Drop the redundant 'country' column after each merge
 
-    # Handle missing values (you might want to use a more sophisticated method depending on your data)
-    # final_df = final_df.dropna()
-    final_df = final_df.interpolate()
+    # Handle missing values
+    final_df = final_df.dropna() # Drop rows with missing values
+    # final_df = final_df.interpolate() # Interpolate missing values
 
     # Convert data types as needed
     final_df['total_medal_count'] = final_df['total_medal_count'].astype(int)
     final_df['hosting_status'] = final_df['hosting_status'].astype(int)
-    final_df['population'] = final_df['population'].astype(float)
+    final_df['population'] = final_df['population'].astype(int)
     final_df['gdp_per_capita'] = final_df['gdp_per_capita'].astype(float)
     final_df['life_expectancy'] = final_df['life_expectancy'].astype(float)
     final_df['pop_20_39_percent'] = final_df['pop_20_39_percent'].astype(float)
@@ -75,7 +77,7 @@ def main():
     print(final_df.info())
 
     # Save the final dataframe to a CSV file
-    final_df.to_csv('olympic_analysis_data_interpolated.csv', index=False)
+    final_df.to_csv('olympic_analysis_data.csv', index=False)
 
 if __name__ == '__main__':
     main()
